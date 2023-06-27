cf55d5a77f9ce1006de650855d5831675f83bd76
SurfaceTens <sergio_perea_91@hotmail.com>
2023-06-27 10:26:19 +0200

    ConImagen usa array de bytes

--- lib/src/main/java/es/mde/acing/utils/ConImagen.java
+++ lib/src/main/java/es/mde/acing/utils/ConImagen.java
@@ -4,12 +4,6 @@ import es.mde.acing.utils.PreguntaImpl.Adjunto;
 
 public interface ConImagen {
 
-	String getImagenBase64();
-
-	void setImagenBase64(String imagenBase64);
-
-	boolean esImagenValida();
-
 	byte[] getImagen();
 
 	void setImagen(byte[] imagen);
--- lib/src/main/java/es/mde/acing/utils/PreguntaConImagen.java
+++ lib/src/main/java/es/mde/acing/utils/PreguntaConImagen.java
@@ -1,26 +1,10 @@
 package es.mde.acing.utils;
 
-import java.util.Base64;
+import java.util.Arrays;
 
 public class PreguntaConImagen extends PreguntaImpl implements ConImagen {
 
 	private byte[] imagen;
-
-	@Override
-	public String getImagenBase64() {
-		return Base64.getEncoder().encodeToString(imagen);
-	}
-
-	@Override
-	public void setImagenBase64(String imagenBase64) {
-		this.imagen = Base64.getDecoder().decode(imagenBase64);
-	}
-	
-	@Override
-	public boolean esImagenValida() {
-		// No es nula y no esta vacia
-        return imagen != null && imagen.length > 0;
-    }
 
 	@Override
 	public byte[] getImagen() {
@@ -39,7 +23,7 @@ public class PreguntaConImagen extends PreguntaImpl implements ConImagen {
 
 	@Override
 	public String toString() {
-		return getImagenBase64();
+		return "PreguntaConImagen [imagen=" + Arrays.toString(imagen) + "]";
 	}
 
 }
